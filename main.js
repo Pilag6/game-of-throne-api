6bc6c70a0d6742cd351b9eadb62b6afe1be7cb17
Pilag6 <pilag6@gmail.com>
2023-12-03 14:35:31 +0100

    Add character images and update CSS styles

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const houseUrl = "https://api.gameofthronesquotes.xyz/v1/houses";
 const personUrl = "https://api.gameofthronesquotes.xyz/v1/characters";
 const quoteUrl = "https://api.gameofthronesquotes.xyz/v1/quotes";
+const authorUrl = "https://api.gameofthronesquotes.xyz/v1/authors";
 
 // Function to fetch and display houses
 function fetchAndDisplayHouses() {
     fetch(houseUrl)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
+            // console.log(data);
             const houseList = document.querySelector("#houses ul");
 
             data.forEach((house) => {
@@ -47,57 +48,58 @@ function addHouseLinksEventListeners(data) {
 
 // Function to fetch and display persons
 function displayPersons(house) {
-    const personList = document.querySelector("#persons ul");
+    const personList = document.querySelector("#persons");
     personList.innerHTML = `
+        <h2 class="house-name-person">Persons</h2>
         <h3 class="house-name">${house.name}</h3>
         <ul class="house-person">
             ${house.members
                 .map(
                     (member) => `
-                        <li>
+                        <li style="background-image: url(./img/characters/${member.slug}.jpg);">
                             <a href="#${member.slug}">${member.name}</a>
                         </li>
                     `
                 )
                 .join("")}
         </ul>
-        <a href="#persons" class="see-all-btn">See all characters</button>
-    `;
+        `;
+
+    // Add event listeners to person links
+
+    const personLinks = document.querySelectorAll("#persons ul li a");
+    personLinks.forEach((link) => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+            const slug = link.getAttribute("href").slice(1);
+            // console.log(slug);
+            const person = house.members.find((member) => member.slug === slug);
+            console.log(person);
+            displayPersonDetails(person);
+        });
+    });
 
     // Scroll until persons section
     const personsSection = document.querySelector("#persons");
     personsSection.scrollIntoView({
         behavior: "smooth",
     });
-
-    const seeAllBtn = document.querySelector(".see-all-btn");
-
-    seeAllBtn.addEventListener("click", (event) => {
-        event.preventDefault();
-        const personList = document.querySelector("#persons ul");
-
-        personList.scrollIntoView({
-            behavior: "smooth",
-        });
-
-        fetchAndDisplayPersons();
-    });
 }
 
 // Function to fetch and display persons
-function fetchAndDisplayPersons() {
-    fetch(personUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            const personList = document.querySelector("#persons ul");
+// function fetchAndDisplayPersons() {
+//     fetch(personUrl)
+//         .then((response) => response.json())
+//         .then((data) => {
+//             // console.log(data);
+//             const personList = document.querySelector("#persons ul");
 
-            data.forEach((person) => {
-                const personItem = createPersonItem(person);
-                personList.appendChild(personItem);
-            });
-        });
-}
+//             data.forEach((person) => {
+//                 const personItem = createPersonItem(person);
+//                 personList.appendChild(personItem);
+//             });
+//         });
+// }
 
 // Function to create a person item
 function createPersonItem(person) {
@@ -128,7 +130,51 @@ function createQuoteItem(quote) {
     return quoteItem;
 }
 
-// Add a modal to show quotes on click in each person through the slug in the API
+// Function to display person details
+
+function displayPersonDetails(person) {
+    fetch(personUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            // DIsplay only the quotes of the selected person. The quotes are in the data array
+
+            const personQuotes = data.filter(
+                (quote) => quote.slug === person.slug
+            );
+            console.log("Person Quotes", personQuotes[0].quotes);
+
+            // Display the person details
+
+            const personDetails = document.querySelector("#person-details");
+            personDetails.innerHTML = `
+                    <div class="person-details-container">
+                        <h2 class="person-details-name">${person.name}</h2>
+                        <article>
+                            <img src="./img/characters/${
+                                person.slug
+                            }.jpg" alt="">
+                            <div class="person-quotes">
+                                <h3>Quotes</h3>
+                                <ul>
+                                    ${personQuotes[0].quotes
+                                        .map((quote) => `<li>* ${quote}</li>`)
+                                        .join("")}
+                                </ul>
+                            </div>
+                        </article>
+                    </div>
+            `;
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+    // Scroll until persons section
+    const personDetailsName = document.querySelector(".person-details-name");
+    personDetailsName.scrollIntoView({
+        behavior: "smooth",
+    });
+}
 
 // Initial function calls
 fetchAndDisplayHouses();
